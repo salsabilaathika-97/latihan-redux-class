526fc9bc5a94d174974e29de9cf2e735253b6d9b
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-01 22:21:33 +0800

    display list user again in home page

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -37,6 +37,14 @@ const Home = () => {
                 <button>Register</button>
             </Link>
             <h1>Home Page</h1>
+            {
+                dataUser.data.map(item => (
+                    <div>
+                        <h1>{item.first_name}</h1>
+                        <img src = {item.avatar} />    
+                    </div>
+                ))
+            }
         </div>
     )
 }
