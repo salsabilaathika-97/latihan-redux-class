a0e7c56eaa640039b1d60c895a613309108991c9
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-13 17:05:20 +0800

    change api url to mock api url

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {useEffect} from "react";
-import { getData } from "../redux/actions/dataAction";
+import { getData, getMockData } from "../redux/actions/dataAction";
 
 const Card = () => {
     const dispatch = useDispatch();
     const {dataUser} = useSelector((state)=> state);
     
     useEffect(() => {
-        dispatch(getData());
+        dispatch(getMockData());
     }, []);
+
+    const formatCurrency = (number) => {
+        let fNumber = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        return "Rp. "+fNumber;
+    }
     return (
       <div>
            {
@@ -17,6 +22,8 @@ const Card = () => {
                     <div style={{ padding: 10 }}>
                             <div className="card" style={{padding: 25}}>
                                 <img className="rounded-t-lg" src={item.image} style={{maxHeight: 160, maxWidth: 270}} alt="Gambar Mobil" />
+                                <p>Nama/Tipe Mobil</p>
+                                <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>{formatCurrency(item.price) + " / hari"}</div>
                                 <div className="tracking-tight text-gray-900 dark:text-white" style={{marginTop: 16}}>{item.name}</div>
                                     <div className="inline-flex justify-content-center py-2 px-3 text-sm font-bold text-center text-white bg-green-500 w-100 rounded-lg hover:bg-green-700 focus:ring-4 focus:outline-none">
                                         Pilih Mobil
--- src/redux/actions/dataAction.js
+++ src/redux/actions/dataAction.js
@@ -12,3 +12,16 @@ export const getData = () => (dispatch) => {
       })
       .catch((err) => console.log(err));
 }
+
+export const getMockData = () => (dispatch) => {
+    axios
+    .get("http://localhost:3001/admin/car")
+    .then((res) => {
+      dispatch({
+          type: TYPES.FETCH_DATA,
+          payload: res.data,
+      })
+      console.log(res.data);
+    })
+    .catch((err) => console.log(err));
+}
