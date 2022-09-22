4e1c752b27feb2aee165859ad50fc5294da0a23b
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-22 22:36:18 +0800

    change handle car api

--- src/redux/actions/carAction.js
+++ src/redux/actions/carAction.js
@@ -2,13 +2,13 @@ import axios from "axios"
 import TYPES from "../types"
 
 export const handleCar = (id) => (dispatch) => {
-    axios
-      .get(`https://bootcamp-rent-car.herokuapp.com/admin/car/${id}`)
-      .then((res) => {
-        dispatch({
-          type: TYPES.GET_CARS,
-          payload: res.data,
-        })
-      })
-      .catch((err) => console.log(err.message))
+  axios
+  .get("https://bootcamp-rent-car.herokuapp.com/admin/car")
+  .then((res) => {
+    dispatch({
+      type: TYPES.GET_CARS,
+      payload: res.data,
+    })
+  })
+  .catch((err) => console.log(err.message))
   }
