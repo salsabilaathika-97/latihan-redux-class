2d7c367813b4000468a3d43cc4f521247a9b627a
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-22 19:33:48 +0800

    install flowbite css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-green': '#5CB85F',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
